a1b8aa67b069ce9d38e4978832bf4b723d46390c
Jose <jose.zunigahenriquez@gmail.com>
2022-07-28 12:06:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Todos from './components/todos/Todos';
+import UserContextProvider from './providers/UserContextProvider';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <UserContextProvider>
+      <Header/>
+      <Profile/>
+      <Todos/>
+    </UserContextProvider>
   );
 }
 
